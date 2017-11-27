71c1a629b82aeac929cc3ea08f8554789759f5b9
FangzhouXiong <matthewxfz@gmail.com>
2017-11-26 21:22:04 -0600

    up plot with kmean

--- src/plotModel.py
+++ src/plotModel.py
@@ -18,7 +18,7 @@ dirpath = os.path.dirname(os.path.realpath(__file__))
 
 
 
-sys.path.append(dirpath)
+sys.path.append("/Users/matthewxfz/Workspaces/gits/course/YelpReivewSentimentAnalysis/src")
 import util
 
 #dirpath = "/Users/matthewxfz/Workspaces/gits/course/YelpReivewSentimentAnalysis/data"
@@ -26,12 +26,13 @@ outputfile = dirpath + "/wv_embendded.csv"
 modelpath = dirpath + "/yelpLtraining";
 outpufpng = dirpath + "/wcloud.png"
 
-def plot_with_labels(low_dim_embs, labels, filename=outpufpng,fonts=None):
+def plot_with_labels(low_dim_embs, labels, idx, filename='images/tsne3.png',fonts=None):
     assert low_dim_embs.shape[0] >= len(labels), "More labels than embeddings"
-    plt.figure(figsize=(25, 25))  # in inches
+    plt.figure(figsize=(18, 18))  # in inches
     for i, label in enumerate(labels):
         x, y = low_dim_embs[i, :]
-        plt.scatter(x, y)
+        color = idx[i];
+        plt.scatter(x, y, c=color)
         plt.annotate(label,
                     fontproperties=fonts,
                     xy=(x, y),
@@ -39,7 +40,7 @@ def plot_with_labels(low_dim_embs, labels, filename=outpufpng,fonts=None):
                     textcoords='offset points',
                     ha='right',
                     va='bottom')
-        print(i);
+
     plt.savefig(filename,dpi=800)
     
 #%%
@@ -59,6 +60,26 @@ for line in file:
 wv_arr = np.array(wv_arr, dtype = float)
 #%%
 
+#%% cluster the model
+from sklearn.cluster import KMeans
+import time
+
+start = time.time() # Start time
+
+# Set "k" (num_clusters) to be 1/5th of the vocabulary size, or an
+# average of 5 words per cluster
+word_vectors = model.wv.syn0
+num_clusters = 50;
+
+# Initalize a k-means object and use it to extract centroids
+kmeans_clustering = KMeans( n_clusters = num_clusters, max_iter = 5, n_jobs = 10,  )
+idx = kmeans_clustering.fit_predict( word_vectors )
+
 print("start printing!")
-plot_with_labels(wv_arr , model.wv.index2word);
+plot_with_labels(wv_arr[0:10,:], model.wv.index2word[0:10],idx[0:10]);
 print("end printing!")
+
+# Get the end time and print how long the process took
+end = time.time()
+elapsed = end - start
+print "Time taken for K Means clustering: ", elapsed, "seconds."
